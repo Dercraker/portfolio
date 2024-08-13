ea7c3027754eb52df16aa82f519f267f3b08c739
Dercraker <antoine.capitain@gmail.com>
2024-08-13 17:48:27 +0200

    clone sample
    J'ai la flem de faire le setup de ses mort

---
+++ src/site-config.ts
@@ -0,0 +1,27 @@
+export const SiteConfig = {
+  title: "Portfolio - Dercraker",
+  description: "Portfolio de Dercraker - Alias Antoine",
+  prodUrl: "https://portfolio.dercraker.fr",
+  domain: "portfolio.dercraker.fr",
+  appIcon: "/images/icon.png",
+  company: {
+    name: "Dercraker Inc.",
+    address: "In the clouds",
+  },
+  brand: {
+    primary: "#007291",
+  },
+  email: {
+    from: "NowTS <nowts@resend.dev>", // Replace this with your domaine email
+    contact: "contact@nowts.app",
+  },
+  maker: {
+    image: "https://melvynx.com/images/me/twitter-en.jpg",
+    website: "https://dercraker.fr",
+    twitter: "https://twitter.com/melvyn_me",
+    name: "Melvynx",
+  },
+  auth: {
+    password: false as boolean,
+  },
+};
